15eb65e827f5c0e08493f1fda032090b8f4d937f
Work Goonja <workgoonja@goonjas-MacBook-Pro.local>
2025-06-08 13:32:36 +0530

    app was missplaced in fact-checking-api.js just corrected that

--- backend/fact-checking-api.js
+++ backend/fact-checking-api.js
@@ -2,8 +2,8 @@ import express from 'express';
 import classifyRoutes from './routers/classifyRouter.js';
 import cors from 'cors';
 
-app.use(cors());
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', classifyRoutes);
